4f8b63d54474672fd9b33f40336e9cd784d8e455
nhhtrann <ht69224@gmail.com>
2026-04-21 10:51:37 +0700

    up

---
+++ script.js
@@ -0,0 +1,46 @@
+// 1. Khai báo
+const todoInput = document.getElementById('todo-input');
+const addBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+
+// 2. Mảng chứa danh sách công việc
+let tasks = [];
+
+// 3. Hàm hiển thị danh sách
+function renderTasks() {
+    todoList.innerHTML = ''; 
+    
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.className = task.completed ? 'completed' : '';
+        li.innerHTML = `
+            <span>${task.text}</span>
+            <button onclick="deleteTask(${index})">Xóa</button>
+        `;
+        todoList.appendChild(li);
+    });
+}
+
+// 4. Hàm thêm 
+function addTask() {
+    const text = todoInput.value.trim();
+    
+    if (text === "") {
+        alert("Bạn chưa nhập công việc mà!");
+        return;
+    }
+    
+    tasks.push({
+        text: text,
+        completed: false
+    });
+    
+    todoInput.value = ''; 
+    renderTasks(); 
+}
+
+// 5. Hàm xóa (Phải có hàm này thì render mới không bị lỗi khi bấm nút Xóa)
+
+
+// 6. Lắng nghe sự kiện click
+addBtn.addEventListener('click', addTask);
